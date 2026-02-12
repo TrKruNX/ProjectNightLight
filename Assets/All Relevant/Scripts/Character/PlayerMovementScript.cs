e99bf567a8d02e6fe1e360c5c34b95954a328951
TrKruNX <davidkarlsen05@gmail.com>
2026-02-12 01:54:37 +0100

    Main Start
    I have imported a few assets like (uv checker - texture), and a skybox shader I made in another project.
    
    Then I made a layout inside the "david" scene and made 2 monobehaviour scripts, these scripts are not nearly finished, but they work, a tiny but, it is the scripts to control the player :]

---
+++ Assets/All Relevant/Scripts/Character/PlayerMovementScript.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+
+    [SerializeField] private Transform camHolder;
+
+    [Header("Player Move")]
+    private float moveSpeed = 9f;
+
+    Rigidbody rb;
+
+    // Start
+    private void Start()
+    {
+        rb = GetComponentInChildren<Rigidbody>();
+
+        // Basically command to freeze rigidbody in specifiq angles "|" <-- this means "and/or"
+        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+    }
+
+    // Update
+    private void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+
+        // Get input from keyboard (WASD or arrow keys)
+        float moveX = Input.GetAxisRaw("Horizontal"); // -1 to 1 (left/right)
+        float moveZ = Input.GetAxisRaw("Vertical");   // -1 to 1 (back/forward)
+
+        // Calculate Move Direction
+        Vector3 move = new Vector3(moveX, 0f, moveZ);
+
+
+        // Move the character
+        // Time.deltaTime ensures smooth, consistent movement regardless of frame rate
+        transform.Translate(move * moveSpeed * Time.deltaTime);
+
+    }
+}
---
+++ Assets/All Relevant/Scripts/Character/ThirdPersonCamera.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    [Header("Player Logic")]
+    [SerializeField] private Transform playerObj;
+
+
+    [Header("camLogic")]
+    [SerializeField] private float mouseSensitivity = 200f;
+    [SerializeField] private float distance = 10f;
+    [SerializeField] private LayerMask defaultLayer;
+
+    
+    [SerializeField] private float minY = -30f; // lowest camera angle
+    [SerializeField] private float maxY = 65f;  // highest camera angle
+
+    private float mouseX;
+    private float mouseY;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        mouseX += Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouseY -= Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        mouseY = Mathf.Clamp(mouseY, minY, maxY);
+
+    }
+
+    void LateUpdate()
+    {
+        Quaternion rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        Vector3 offset = rotation * Vector3.back * distance;
+
+        transform.position = playerObj.position + offset;
+        transform.LookAt(playerObj.position);
+    }
+
+
+    // This doesnt work just yes, but it will be way to prevent clipping in the prototyping
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 0)
+        {
+            mouseSensitivity = -200f;
+            Debug.Log("Layer Is detected");
+        }
+        else
+        {
+            mouseSensitivity = 200f;
+        }
+
+        if (collision.gameObject.CompareTag("Default"))
+        {
+            mouseSensitivity = -200f;
+            Debug.Log("Layer Is detected");
+        }
+        else
+        {
+            mouseSensitivity = 200f;
+        }
+    }
+
+}
